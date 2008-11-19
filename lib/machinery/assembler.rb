bba0fb736b26390c560e85e50c08876530a039c7
Arto Bendiken <arto@bendiken.net>
2008-11-19 10:36:05 +0100

    Implemented the assembler factory.

--- lib/machinery/assembler.rb
+++ lib/machinery/assembler.rb
@@ -1,5 +1,44 @@
 module Machinery
   class Assembler
-    # TODO
+
+    ##
+    # Assembles a sequence of instructions for the specified target.
+    #
+    # @param insns    a sequence of assembly instructions
+    # @param target   the assembler's target name (e.g. :pyc)
+    # @param options  options for the assembler
+    def self.assemble(insns, options = {})
+      target = @@targets.index(self) || options[:target]
+      raise ArgumentError.new('must specify :target in arguments') unless target
+      self.for(target).new.assemble(insns, options)
+    end
+
+    ##
+    # Returns an assembler implementation for the specified target.
+    #
+    # @param target   the assembler's target name (e.g. :pyc)
+    # @return [Class] the assembler class
+    def self.for(target)
+      require "machinery/assembler/#{target}"
+      @@targets[target.to_sym]
+    end
+
+    ##
+    # Assembles a sequence of instructions into a target-specific result.
+    #
+    # @param insns    a sequence of assembly instructions
+    # @param options  options for the assembler
+    def assemble(insns, options = {})
+      raise NotImplementedError.new
+    end
+
+    protected
+      @@targets = {}
+
+      def self.target(name) #:nodoc:
+        @@targets[name.to_sym] = self
+      end
+
+    module Targets end
   end
 end
