b6aadf11e7ffa816bdf7a4a8656e130c5fa334af
Arto Bendiken <arto@bendiken.net>
2008-12-06 16:23:00 +0100

    Implemented Machinery::Architecture::Register and Machinery::Architecture::Stack.

--- lib/machinery/arch.rb
+++ lib/machinery/arch.rb
@@ -1,7 +1,14 @@
 module Machinery
+  ##
+  # Machine components and architectures.
+  #
+  # @see http://en.wikipedia.org/wiki/Computer_architecture
   module Architecture
 
     ##
+    # A machine instruction.
+    #
+    # @see http://en.wikipedia.org/wiki/Instruction_(computer_science)
     class Instruction
       attr_accessor :opcode, :operands
 
@@ -17,12 +24,16 @@ module Machinery
         operands.length
       end
 
-      def to_a  
-        [opcode.to_i, *operands.map(&:to_i)]     
+      def to_sxp() to_a end
+
+      def to_a
+        #[opcode.to_i, *operands.map(&:to_i)]
+        [opcode, *operands]
       end
 
-      def to_s               
-        to_a.pack('c*')
+      def to_s
+        #to_a.pack('c*')
+        (opcode.to_s + ' ' + operands.map(&:to_s).join(', ')).strip
       end
 
       def each_byte(&block)
@@ -40,11 +51,63 @@ module Machinery
       def write(out)
         out << to_s
       end
+
+      def inspect
+        "#<#{self.class}(#{to_s})>"
+      end
     end
 
     ##
+    # A machine register.
+    #
+    # @see http://en.wikipedia.org/wiki/Processor_register
+    class Register
+      attr_accessor :name, :value
+
+      def initialize(name, value = nil)
+        @name, @value = name.to_sym, value
+      end
+
+      def to_sxp() name end
+      def to_s()   name.to_s end
+
+      def inspect
+        "#<#{self.class}(#{to_s})>"
+      end
+    end
+
+    ##
+    # A machine stack.
+    #
+    # @see http://en.wikipedia.org/wiki/Stack_(data_structure)
+    class Stack
+      attr_accessor :name, :elements
+
+      def initialize(name, *elements)
+        @name, @elements = name.to_sym, elements.flatten
+      end
+
+      def push(*args)
+        @element.push(*args)
+      end
+
+      def pop
+        @elements.pop
+      end
+    end
+
+    ##
+    # A stack machine architecture.
+    #
+    # @see http://en.wikipedia.org/wiki/Stack_machine
+    # @see http://www.ece.cmu.edu/~koopman/stack_computers/
     module StackMachine
       @@stack_effects = {}
+
+      def stack(name, options = {})
+        stack = Stack.new(name = name.to_sym)
+        define_method(name) { stack }
+      end
 
       def defop(opname, stack_effects = {}, &block)
         @@stack_effects ||= {}
@@ -65,6 +128,14 @@ module Machinery
     end
 
     ##
-    module RegisterMachine end
+    # A register machine architecture.
+    #
+    # @see http://en.wikipedia.org/wiki/Register_machine
+    module RegisterMachine
+      def register(name, options = {})
+        register = Register.new(name = name.to_sym)
+        define_method(name) { register }
+      end
+    end
   end
 end
